b617bd04acbb0dd5c4c454534dbab8bc8e8be2fd
HARIN-YU <book357610@gmail.com>
2023-02-16 22:33:15 +0900

    수정 사항

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Button = styled.button`
   border: 1px solid blue;
   border-radius: 5px;
-  background-color: skyblue;
+  background-color: teal;
   color: blue;
   width: 200px;
   cursor: pointer;
