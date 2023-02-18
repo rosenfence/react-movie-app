072697830f048d7c78fc9e355c7127b1773612c1
rosenfence <rosenfence@gmail.com>
2023-02-18 20:27:48 +0900

    fix: 기존 컴포넌트 스타일에 맞게 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const LayoutDiv = styled.div`
   display: flex;
-  justify-content: center;
+  align-items: center;
   flex-direction: column;
   max-width: 1250px;
 `;
